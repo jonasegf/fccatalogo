8f0816dfe28798b703e603833fd31db1ef5fcd31
jonasegf <jonas.edu@hotmail.com>
2022-01-31 16:36:47 -0300

    Criação BaseEntity

--- src/main/java/com/fullcycle/FCCatalogo/domain/entity/Category.java
+++ src/main/java/com/fullcycle/FCCatalogo/domain/entity/Category.java
@@ -2,37 +2,34 @@ package com.fullcycle.FCCatalogo.domain.entity;
 
 import java.util.UUID;
 
-public class Category {
-  private UUID id;
-  private String name;
+public class Category extends BaseEntity {
 
-  public Category(UUID id, String name) {
-    this.id = id;
-    this.name = name;
-  }
+    private String name;
 
-  public Category(String name) {
-    this.name = name;
-  }
+    public Category() {
+    }
 
-  public Category() {
-  }
+    public Category(String name) {
+        super.generateUUID();
+        this.setName(name);
+    }
 
-  public UUID getId() {
-    return id;
-  }
+    public Category(UUID id) {
+        super.setId(id);
+    }
 
-  public void setId(UUID id) {
-    this.id = id;
-  }
+    public Category(UUID id, String name) {
+        super.setId(id);
+        this.setName(name);
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    if (name == null) throw new IllegalArgumentException("");
-    if (name.length() == 0) throw new IllegalArgumentException("");
-    this.name = name;
-  }
+    public void setName(String name) {
+        if (name == null) throw new IllegalArgumentException("");
+        if (name.length() == 0) throw new IllegalArgumentException("");
+        this.name = name;
+    }
 }
